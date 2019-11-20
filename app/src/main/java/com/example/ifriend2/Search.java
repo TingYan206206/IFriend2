d641167c937763f208e273a1eccc6088de499d59
Ting Yan <tingyan206206@gmail.com>
2019-11-19 19:36:37 -0600

    add dropdown selector in search page

--- app/src/main/java/com/example/ifriend2/Search.java
+++ app/src/main/java/com/example/ifriend2/Search.java
@@ -3,12 +3,54 @@ package com.example.ifriend2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class Search extends AppCompatActivity {
+public class Search extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private Spinner dropdown;
+    private static final String[] levels = new String[]{"Undergrad", "Master", "Doctor"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        dropdown = findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, levels);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                Log.i("position:", position + " value: " + levels[position]);
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                Log.i("position:", position + " value: 2");
+
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                Log.i("position:", position + " value: three");
+
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+
 }
